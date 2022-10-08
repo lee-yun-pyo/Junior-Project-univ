15d4652ee551403fb0f49cfbf986875c2d0a678a
YTim <yun2193@hanmail.net>
2022-10-09 02:19:53 +0900

    #7 Login 화면 및 기능 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "회원 가입" });
@@ -17,15 +18,45 @@ export const postJoin = async (req, res) => {
       .status(400)
       .render("join", { pageTitle, errorMessage: "password가 다릅니다" });
   }
-  await User.create({
-    email,
-    password,
-    name,
-  });
-  res.redirect("/login");
+  try {
+    await User.create({
+      email,
+      password,
+      name,
+    });
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+    res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const pageTitle = "Login";
+  // * 이메일 있는지 확인
+  // 비번 확인
+  const user = await User.findOne({ email }); // 있으면 object 반환 없으면 null
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "이메일 존재 하지 않음",
+    });
+  }
+  const isPassword = await bcrypt.compare(password, user.password); // 맞지 않으면 false 맞 true
+  if (!isPassword) {
+    return res
+      .status(400)
+      .render("login", { pageTitle, errorMessage: "password 틀림" });
+  }
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("User edit");
-export const login = (req, res) => res.send("user login");
 export const remove = (req, res) => res.send("delete user");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("user Seee");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { home, search } from "../controllers/postController";
-import { getJoin, postJoin, login } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userController";
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.route("/join").get(getJoin).post(postJoin);
-globalRouter.get("/login", login);
+globalRouter.route("/login").get(getLogin).post(postLogin);
 globalRouter.get("/search", search);
 
 export default globalRouter;
