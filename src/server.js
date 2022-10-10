96f291ac5928b57f7305950245805f2296067c1a
YTim <yun2193@hanmail.net>
2022-10-11 01:13:26 +0900

    #7 process.env 생성 & resave: false, saveUninitialized: false,

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import postRouter from "./routers/postRouter";
@@ -21,9 +22,12 @@ app.use(express.urlencoded({ extended: true }));
 /* session 처리 middleware */
 app.use(
   session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URL,
+    }), // mongoDB에 session 저장
   })
 );
 
