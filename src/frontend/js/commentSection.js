1c5e204cf6482ddabd2c9f36c625efaba8d5594e
YTim <yun2193@hanmail.net>
2022-11-25 16:38:13 +0900

    #FE 댓글 CSS 작업

--- src/frontend/js/commentSection.js
+++ src/frontend/js/commentSection.js
@@ -6,10 +6,23 @@ const btn = form.querySelector("button");
 const addComment = (text) => {
   const postComments = document.querySelector(".post__comments ul");
   const newComment = document.createElement("li");
+  const btnDiv = document.createElement("div");
+  const editBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+  const editIcon = document.createElement("i");
+  const deleteIcon = document.createElement("i");
   newComment.className = "post__comment";
+  btnDiv.className = "post__comment-btn";
+  editIcon.className = "fa-regular fa-pen-to-square";
+  deleteBtn.className = "fa-solid fa-trash-can";
+  editBtn.appendChild(editIcon);
+  deleteBtn.appendChild(deleteIcon);
+  btnDiv.appendChild(editBtn);
+  btnDiv.appendChild(deleteBtn);
   const span = document.createElement("span");
   span.innerText = `${text}`;
   newComment.appendChild(span);
+  newComment.appendChild(btnDiv);
   postComments.prepend(newComment);
 };
 
